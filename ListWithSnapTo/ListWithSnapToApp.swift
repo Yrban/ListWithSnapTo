6bd0dd551a97948195f70a9a9e3be41ad85cb2f2
Alelin Apps <alelinapps@gmail.com>
2022-05-22 10:30:38 -0400

    Initial Commit

---
+++ ListWithSnapTo/ListWithSnapToApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithSnapToApp.swift
+//  ListWithSnapTo
+//
+//  Created by Developer on 5/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ListWithSnapToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
